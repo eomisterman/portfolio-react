0ab57e66a8e0cecf081fbb4eb9e520bb4bdcf24e
emilio ovalles-misterman <eomisterman@gmail.com>
2023-10-11 11:03:43 -0400

    Revised about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
             <section class="min-h-fit basis-auto my-10 mr-7 text-sm font-mono text-slate-700 max-w-lg">
                 <h2 class="text-3xl sm:text-2xl font-extrabold mb-16">About</h2>
                 <div class="mb-16">
-                    <p>Emilio Ovalles-Misterman. Software Engineer. Photographer. Tinkerer. Aspiring hacker. Creator of things. I like to explore many different mediums and technologies and I can't resist leaving a personal project unfinished.</p>
+                    <p>Emilio Ovalles-Misterman, a multifaceted Software Engineer, Photographer, and Tinkerer with a passion for hacking and creating innovative solutions. I thrive on exploring diverse mediums and cutting-edge technologies to bring ideas to life.</p>
                 </div>
             </section>
         </>
